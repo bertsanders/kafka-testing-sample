71f99205cdefbb162e27ba35d1f43c04ecddb641
b0s01ar <paul mcb -18>
2019-11-20 13:11:16 -0600

    create an initial project

---
+++ src/main/java/com/walmart/demo/kafka/KafkaTestingSampleApplication.java
@@ -0,0 +1,13 @@
+package com.walmart.demo.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaTestingSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaTestingSampleApplication.class, args);
+  }
+
+}
