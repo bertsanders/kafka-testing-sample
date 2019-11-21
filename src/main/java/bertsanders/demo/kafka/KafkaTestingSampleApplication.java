369e34d6746c794452bfc20a537e0463ff4f8114
b0s01ar <paul mcb -18>
2019-11-21 14:10:48 -0600

    initial project

--- src/main/java/bertsanders/demo/kafka/KafkaTestingSampleApplication.java
+++ src/main/java/bertsanders/demo/kafka/KafkaTestingSampleApplication.java
@@ -1,4 +1,4 @@
-package com.walmart.demo.kafka;
+package bertsanders.demo.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
