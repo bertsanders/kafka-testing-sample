dedc9d117f5687d98d0b9983f0c13155821df8d3
b0s01ar <paul mcb -18>
2019-11-21 10:46:25 -0600

    cleanup this test

--- src/test/java/com/walmart/demo/kafka/ExpensiveServiceTest.java
+++ src/test/java/com/walmart/demo/kafka/ExpensiveServiceTest.java
@@ -1,5 +1,7 @@
 package com.walmart.demo.kafka;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.math.BigInteger;
 
 import org.junit.jupiter.api.Test;
@@ -10,12 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 class ExpensiveServiceTest {
 
   @Test
-  void factorial() {
+  void primes() {
     ExpensiveService service = new ExpensiveService();
     for (int i = 0; i < 10; ++i) {
       int input = (int) (Math.random() * 2000.0);
-      BigInteger factorial = service.nthPrime(input);
-      log.info("{}, {}", input, factorial.toString());
+      BigInteger prime = service.nthPrime(input);
+      assertThat(prime.isProbablePrime(10)).isTrue();
+      log.debug("{}, {}", input, prime.toString());
     }
   }
 }
